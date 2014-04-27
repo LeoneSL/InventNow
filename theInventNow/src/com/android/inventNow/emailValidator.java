de7a9840d80b924413a012c62ff0a71a32a2f4ef
LeoneSL <gil.jmacauley@gmail.com>
2014-04-26 22:03:18 -0400

    sixth commit- updated uinventnow

--- theInventNow/src/com/android/inventNow/emailValidator.java
+++ theInventNow/src/com/android/inventNow/emailValidator.java
@@ -1,5 +1,28 @@
 package com.android.inventNow;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class emailValidator {
+	
+	/**
+	 * method is used for checking valid email id format.
+	 * 
+	 * @param email
+	 * @return boolean true for valid false for invalid
+	 */
+	public static boolean isEmailValid(String email) {
+	    boolean isValid = false;
+
+	    String expression = "^[\\w\\.-]+@([\\w\\-]+\\.)+[A-Z]{2,4}$";
+	    CharSequence inputStr = email;
+
+	    Pattern pattern = Pattern.compile(expression, Pattern.CASE_INSENSITIVE);
+	    Matcher matcher = pattern.matcher(inputStr);
+	    if (matcher.matches()) {
+	        isValid = true;
+	    }
+	    return isValid;
+	}
 
 }
